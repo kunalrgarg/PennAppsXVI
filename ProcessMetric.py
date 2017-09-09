f8ae6a62658899f7a496ad83d1e2267d797c9868
Jonathan Marty <jonmarty@huntsman-ve702-4007.apng.wlan.private.upenn.edu>
2017-09-08 23:51:52 -0400

    Used inspect to import all methods from Metrics.py

--- ProcessMetric.py
+++ ProcessMetric.py
@@ -1,9 +1,14 @@
 from yahoo_finance import Share
+import inspect
 
 def process_metric(stock_name):
+    metrics = collect_metrics()
     data = Share(stock_name)
     results = [metric(data) for metric in metrics]
     images = [make_img(result) for result in results]
     return images
+
+def collect_metrics():
+    return [member[1] for member in inspect.members()]
 
 def make_img(results):
