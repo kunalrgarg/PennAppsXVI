f159bffd295420a48eabb583f975c47fd4fac0ff
Jonathan Marty <jonmarty@huntsman-ve702-4007.apng.wlan.private.upenn.edu>
2017-09-09 00:45:19 -0400

    Set up the outline for the NASDAQ dozen

--- Metrics.py
+++ Metrics.py
@@ -1,6 +1,38 @@
 #TODO: Get yahoo_finance to read out metrics
 #TODO: Lay out the basic framework for the NASDAQ 12
 
-def get_value(x):
-    print(x)
-    return x.get_price()
+def Revenue(x): #http://www.nasdaq.com/investing/dozen/revenue.aspx
+    pass
+
+def EarningsPerShare(x): #http://www.nasdaq.com/investing/dozen/earnings-per-share.aspx
+    pass
+
+def ReturnOnEquity(x): #http://www.nasdaq.com/investing/dozen/return-on-equity-roe.aspx
+    pass
+
+def AnalystRecommendation(x): #http://www.nasdaq.com/investing/dozen/analyst-recommendations.aspx
+    pass
+
+def PositiveEarningsSurprises(x): #http://www.nasdaq.com/investing/dozen/positive-earnings-surprises.aspx
+    pass
+
+def EarningsForecast(x): #http://www.nasdaq.com/investing/dozen/earnings-forecast.aspx
+    pass
+
+def EarningsGrowth(x): #http://www.nasdaq.com/investing/dozen/earnings-growth.aspx
+    pass
+
+def PEGRatio(x): #http://www.nasdaq.com/investing/dozen/peg-ratio.aspx
+    pass
+
+def IndustryPriceEarnings(x): #http://www.nasdaq.com/investing/dozen/industry-price-earnings.aspx
+    pass
+
+def DaysToCover(x): #http://www.nasdaq.com/investing/dozen/days-to-cover.aspx
+    pass
+
+def InsiderTrading(x): #http://www.nasdaq.com/investing/dozen/insider-trading.aspx
+    pass
+
+def WeightedAlpha(x): #http://www.nasdaq.com/investing/dozen/weighted-alpha.aspx
+    pass
